d55f6a411fd358f7296e153a735f8611608c1ab4
Tedros <tedtfu@gmail.com>
2025-08-14 19:27:20 -0400

    add user profile store

---
+++ src/store/userProfileStore.js
@@ -0,0 +1,7 @@
+const userProfileSchema = {
+    personalProfile: { age, sex, height, weight, bodyComposition, injuryHistory, activityLevel },
+    fitnessGoals: { primary, secondary, timeline },
+    dietaryPreferences: { cuisines, allergies, dietType, macroTargets },
+    equipmentAvailability: { homeGym, commercialGym, noEquipment },
+    lifestyleAssessment: { sleepPatterns, stressLevel, timeAvailable, recoveryHabits }
+  }
